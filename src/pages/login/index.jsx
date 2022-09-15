a2ec586d66b3162dbdd90ce6285b010e50482055
Nargiz Ramazanova <nargiz.rp@code.local>
2022-09-15 11:15:01 +0400

    validation login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import { TextField, Button } from '@mui/material';
-import { flexbox } from '@mui/system';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 function Index() {
+    const schema = yup.object().shape({
+        email: yup.string().email().required("Email is required"),
+        password: yup.string()
+        .min(8, "Password too short").test("isValidPass", " is not valid", (value, context) => {
+            const hasUpperCase = /[A-Z]/.test(value);
+            const hasLowerCase = /[a-z]/.test(value);
+            const hasNumber = /[0-9]/.test(value);
+            const hasSymbole = /[!@#%&]/.test(value);
+            let validConditions = 0;
+            const numberOfMustBeValidConditions = 3;
+            const conditions = [hasLowerCase, hasUpperCase, hasNumber, hasSymbole];
+            conditions.forEach((condition) =>
+              condition ? validConditions++ : null
+            );
+            if (validConditions >= numberOfMustBeValidConditions) {
+              return true;
+            }
+            return false;
+          })
+        .required('Password is required'),
+    }).required();
+    const { register, handleSubmit } = useForm({
+        resolver: yupResolver(schema),
+    });
+    const handleLogin = (data) => console.log(data);
+
+
+
+
     return (
 
         <div style={{
@@ -18,24 +49,30 @@ function Index() {
                     display: "inline-block",
                     borderRadius: "15px"
                 }}
+                onSubmit={handleSubmit(handleLogin)}
             >
                 <TextField id="standard-basic" label="Email..." variant="standard" type="mail"
                     sx={{
                         display: 'block',
                         mb: 2,
                     }}
+                    name="email"
+                    {...register('email')}
                 />
-                <TextField id="standard-basic" label="Password..." variant="standard" type="password"
+                <TextField id="standard-basic1" label="Password..." variant="standard" type="password"
                     sx={{
                         display: 'block',
                         mb: 2,
                     }}
+                    name="password"
+                    {...register('password')}
                 />
                 <Button variant="contained"
                     sx={{
                         display: 'block',
                         width: 200
                     }}
+                    type="submit"
                 >Login</Button>
             </form>
 
